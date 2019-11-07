716f9778d1c1e1023e63960cbb4eed4f13a08339
Daniel Schreurs <daniedaniel.schreurs@hotmail.com>
2019-11-07 07:25:53 +0100

    :tada: Initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,82 +1,75 @@
 const app = {
-  cacheDom() {
-    this.app = document.getElementById("app");
-    this.eListItems = null;// not yet generated
-    this.eFirstListItems = null;// not yet generated
-  },
-  generateItems() {
-    let sClassName;
-    let sText;
-    let eRuben;
-    for (let i = 1; i <= 200; i += 2) {
-      sText = "";
-      eRuben = "";
-      sClassName = "";
-      if (myLittleCalculator.isPremier(i)) {
-        sClassName = "premier";
-        sText = "premier";
-      } else if (myLittleCalculator.isMultipleOf(i, 3)) {
-        if (myLittleCalculator.isMultipleOf(i, 9)) {
-          sText = "3 et 9";
-          sClassName = "multiple-3-9";
-        } else {
-          sClassName = "multiple-3";
-          sText = "3";
-        }
-      }
-      if (sText !== "") {
-        eRuben = `<div class="ribbon-wrapper"><div class="ribbon">${sText}</div></div>`;
-      }
-      this.app.insertAdjacentHTML('beforeend', `<li class="${sClassName} grid__item">${i}${eRuben}</li>`);
-    }
-    this.eListItems = this.app.querySelectorAll('.grid__item');
-    this.eFirstListItems = this.app.querySelectorAll('.premier');
-  },
-  addEventListeners() {
-    this.eFirstListItems.forEach((element) => {
-      element.addEventListener("click", (event) => {
-        this.startAnimation(event);
-      });
-
-      element.addEventListener("transitionend", (event) => {
-        this.stopAnimation(event);
-      })
-    })
-  },
-  startAnimation(event) {
-    event.currentTarget.classList.add("animate");
-    this.eListItems.forEach(function (elem) {
-      elem.classList.add('grid__item--lighter');
-    });
-  },
-  stopAnimation(event) {
-    event.currentTarget.classList.remove("animate");
-    this.eListItems.forEach(function (elem) {
-      elem.classList.remove('grid__item--lighter');
-    });
-  },
   init() {
-    document.documentElement.classList.add("js-enabled");
-    this.cacheDom();
-    this.generateItems();
-    this.addEventListeners();
-  }
-};
+    document.documentElement.classList.add('js-enabled');
+    this.grid = document.getElementById('app');
+    for (let i = 1; i <= 200; i += 2) {
+      const [classes, content] = this.getClassesAndContent(i);
+      const liElt = this.getLiElt(classes, i);
+      this.grid.insertAdjacentElement("beforeend", liElt);
+      if (liElt.classList.contains('premier')) {
+        this.handleEvents(liElt)
+      }
+      liElt.insertAdjacentHTML('beforeend', this.getRibbonHtml(content));
 
-const myLittleCalculator = {
-  isPremier(nbr) {
-    if (nbr < 2) {
-      return false;
     }
-    for (let i = 2; i < nbr; i++) {
-      if (this.isMultipleOf(nbr, i)) {
+  },
+  isPrime(number) {
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
         return false;
       }
     }
     return true;
   },
-  isMultipleOf(base, multiple) {
-    return base % multiple === 0;
+  getLiElt(classes, i) {
+    const liElt = document.createElement('li');
+    liElt.textContent = `${i}`;
+    liElt.classList.add('grid__item');
+    classes.forEach(className => {
+      liElt.classList.add(className);
+    });
+    return liElt;
+  },
+  getRibbonHtml(content) {
+    if (content) return `<div class="ribbon-wrapper">
+                                <div class="ribbon">${content}</div>
+                            </div>`;
+    return '';
+  },
+  getClassesAndContent(i) {
+    let classes = [];
+    let content = '';
+    if (this.isPrime(i) && i > 1) {
+      classes.push('premier');
+      content = 'premier';
+    }
+    if (i % 3 === 0) {
+      classes.push('multiple-3');
+      content = '3';
+    }
+    if (i % 3 === 0 && i % 9 === 0) {
+      classes[classes.length - 1] = 'multiple-3-9';
+      content = '3 et 9';
+    }
+
+    return [classes, content];
+  },
+  handleEvents(liElt) {
+    liElt.addEventListener('click', e => {
+      e.currentTarget.classList.add('animate');
+      this.grid.querySelectorAll('li')
+          .forEach(li =>
+              li.classList.add('grid__item--lighter')
+          )
+    });
+    liElt.addEventListener('transitionend', e => {
+      e.currentTarget.classList.remove('animate');
+      this.grid.querySelectorAll('li')
+          .forEach(li =>
+              li.classList.remove('grid__item--lighter'))
+    });
   }
+
 };
+
 app.init();

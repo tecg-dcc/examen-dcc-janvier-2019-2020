7fb938f0a9f8a9ca2f047e23d732717efb043ac0
Dylan-Jacquet <dylan.jacquet@student.hepl.be>
2023-01-17 16:12:09 +0100

    ✅ Added some improvements

--- main.js
+++ main.js
@@ -1,62 +1,10 @@
 // Nom : JACQUET
 // Prénom : DYLAN
 // Groupe : 286
-
 document.documentElement.classList.add("js-enabled");
+const liContainer = document.querySelector(".grid");
+let listElements = document.querySelectorAll('ol li');
 let start = 1;
-const olSelector = document.querySelector('#app');
-
-const generateItems = function (idx) {
-    for (start; start < idx; start += 2) {
-        if (isPrime(start)) {
-            olSelector.insertAdjacentHTML("beforeend", `<li data-sum="6" class="premier grid__item">${start}
-	                                                      <div class="ribbon-wrapper">
-                                                            <div data-text="somme" class="ribbon">premier</div>
-                                                          </div>
-                                                        </li>`);
-        } else if (start % 3 === 0 && start % 9 === 0) {
-            olSelector.insertAdjacentHTML("beforeend", `<li class="multiple-3-9 grid__item">${start}
-                                                         <div class="ribbon-wrapper">
-                                                            <div class="ribbon">3 et 9</div>
-                                                         </div>
-                                                        </li>`)
-        } else if (start % 3 === 0) {
-            olSelector.insertAdjacentHTML("beforeend", `<li class="multiple-3 grid__item">${start}
-                                                            <div class="ribbon-wrapper">
-                                                                <div class="ribbon">3</div>
-                                                            </div>
-                                                        </li>`)
-        } else {
-            olSelector.insertAdjacentHTML("beforeend", `<li class="grid__item">${start}</li>`)
-        }
-    }
-}
-generateItems(200);
-
-document.addEventListener('scroll', () => {
-    if (window.scrollY + window.innerHeight >= olSelector.scrollHeight) {
-        generateItems(start + 100);
-    }
-});
-
-const liPremier = document.querySelectorAll('.premier');
-const listElements = document.querySelectorAll('ol li');
-console.log(listElements);
-for (const liPremierElement of liPremier) {
-
-    liPremierElement.addEventListener('click', (e) =>{
-        e.currentTarget.classList.add('animate');
-        for (const listElement of listElements) {
-            listElement.classList.add('grid__item--lighter');
-        }
-    });
-    liPremierElement.addEventListener('transitionend', (e) =>{
-        e.currentTarget.classList.remove('animate');
-        for (const listElement of listElements) {
-            listElement.classList.remove('grid__item--lighter');
-        }
-    });
-}
 
 function isPrime(nombre) {
     let prime;
@@ -70,3 +18,75 @@ function isPrime(nombre) {
     }
     return prime;
 }
+
+/* FONCTION RECURSIVE */
+function sum(nb) {
+    if (nb === 0) {
+        return 0;
+    }
+    return nb + sum(nb - 1);
+}
+
+const generateItems = function (number) {
+    for (start; start <= number; start += 2) {
+        if (isPrime(start)) {
+            liContainer.insertAdjacentHTML("beforeend", `<li data-sum="${sum(start)}" class="premier grid__item">${start}
+ 	                                                        <div class="ribbon-wrapper">
+                                                                <div data-text="somme" class="ribbon">premier</div>
+                                                            </div>
+                                                        </li>`);
+        } else if (start % 3 === 0 && start % 9 === 0) {
+            liContainer.insertAdjacentHTML("beforeend", `<li class="multiple-3 grid__item">${start}
+                                                            <div class="ribbon-wrapper">
+                                                                <div class="ribbon">3</div>
+                                                            </div>
+                                                        </li>`);
+        } else if (start % 3 === 0) {
+            liContainer.insertAdjacentHTML("beforeend", `<li class="multiple-3-9 grid__item">${start}
+                                                            <div class="ribbon-wrapper">
+                                                                <div class="ribbon">3 et 9</div>
+                                                            </div>
+                                                        </li>`)
+        } else {
+            liContainer.insertAdjacentHTML("beforeend", `<li class="grid__item">${start}</li>`)
+        }
+    }
+    addAnimation();
+}
+const addAnimation = function(){
+    const liItems = document.querySelectorAll('.grid__item');
+    listElements = document.querySelectorAll('ol li')
+    for (const liItem of liItems) {
+        liItem.addEventListener('click', (e) => {
+            console.log('ça marche')
+            if (e.currentTarget.classList.contains("premier")) {
+                e.currentTarget.classList.add("animate");
+                [e.currentTarget.childNodes[0].textContent, e.currentTarget.dataset.sum] = [e.currentTarget.dataset.sum, e.currentTarget.childNodes[0].textContent];
+                [e.currentTarget.querySelector("[data-text]").dataset.text, e.currentTarget.querySelector("[data-text]").textContent] = [e.currentTarget.querySelector("[data-text]").textContent, e.currentTarget.querySelector("[data-text]").dataset.text];
+                for (const listElement of listElements) {
+                    listElement.classList.add('grid__item--lighter')
+                }
+            }
+
+        });
+        liItem.addEventListener('transitionend', (e) => {
+            if (liItem.classList.contains("animate")) {
+                liItem.classList.remove("animate");
+            }
+            for (const listElement of listElements) {
+                listElement.classList.remove('grid__item--lighter')
+            }
+        });
+    }
+}
+
+generateItems(200);
+document.addEventListener('scroll', (e) => {
+    if (window.scrollY + window.innerHeight >= liContainer.scrollHeight) {
+        generateItems(start + 100);
+    }
+});
+``
+
+
+
